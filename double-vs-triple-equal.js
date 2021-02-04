8f4aaa40f4f0061870744a5ea05248eac0c2eaac
mostafizurrahman01 <abirmostafizurrahman@gmail.com>
2021-02-04 18:22:36 +0600

    doubl equal vs triple equal

---
+++ double-vs-triple-equal.js
@@ -0,0 +1,23 @@
+// double equal diye shudu value gula k check korbe.
+const first = 2;
+// const second = 3;
+const second = "2";
+
+if(first == second){
+    console.log("condition is true");
+}
+else{
+    console.log("condition is false");
+}
+
+
+//triple equal diye value and type check korbe.
+const first = 2;
+const second = "2";
+
+if(first === second){
+    console.log("condition is true");
+}
+else{
+    console.log("condition is false");
+}
