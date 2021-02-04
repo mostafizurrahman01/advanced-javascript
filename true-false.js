27e8dc269d44955af181926b19b49254562570d2
mostafizurrahman01 <abirmostafizurrahman@gmail.com>
2021-02-04 18:00:58 +0600

    true vs false

---
+++ true-false.js
@@ -0,0 +1,22 @@
+// falsy
+// false
+// 0
+// ""
+// undefined
+// null
+// NaN
+// Truthy
+// '0' ' '
+
+
+// const age = 4;
+// const name = "Solaiman"
+// const name = ""
+const name = " ";
+
+if(name){
+    console.log("Condition is true");
+}
+else{
+    console.log("Condition is false");
+}
