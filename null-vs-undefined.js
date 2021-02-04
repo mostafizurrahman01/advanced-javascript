943d442b47237539bc086df1f5debd529f08fffd
mostafizurrahman01 <abirmostafizurrahman@gmail.com>
2021-02-04 18:11:14 +0600

    null vs undefined

---
+++ null-vs-undefined.js
@@ -0,0 +1,20 @@
+let salary;
+console.log(salary);
+
+function add (num1 , num2){
+    console.log(num1 + num2)
+    return
+}
+
+function add (num1 , num2){
+    console.log(num1, num2)
+
+}
+const result = add(13);
+console.log(result);
+
+const person = {name: "Kuddus", phone:4455}
+console.log(person.salary);
+
+let fun = undefined;
+console.log(fun);
